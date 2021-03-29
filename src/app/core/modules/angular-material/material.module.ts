dbb970d6921c00ee4ddd079b3d34f154d91fe7b3
Satvik Gadag <satvikgadag@live.com>
2021-03-29 02:44:39 +0200

    Implemented header, Footer & 404 Page Not Found

--- src/app/core/modules/angular-material/material.module.ts
+++ src/app/core/modules/angular-material/material.module.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -11,12 +12,14 @@ import { MatListModule } from '@angular/material/list';
     MatListModule,
     MatButtonModule,
     MatButtonToggleModule,
+    MatToolbarModule,
   ],
   exports: [
     MatCardModule,
     MatListModule,
     MatButtonModule,
     MatButtonToggleModule,
+    MatToolbarModule,
   ],
 })
 export class MaterialModule {}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MaterialModule } from '../core/modules/angular-material/material.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
